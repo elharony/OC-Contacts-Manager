18977a57c4ca7fd0a08a55925bad210d3e9e26d6
elharony <p.yahyaelharony@gmail.com>
2018-11-24 10:25:53 +0200

    Initial Commit

---
+++ js/contacts.js
@@ -0,0 +1,75 @@
+/* Contact Manager*/
+let contacts = [
+    {
+        firstName: 'John',
+        lastName: 'Smith'
+    },
+    {
+        firstName: 'Jane',
+        lastName: 'Doe'
+    }
+]
+
+
+function init() {
+    // Say Hi!
+    welcomeMessage()
+    // User Options
+    userOptions()
+}
+
+function welcomeMessage() {
+    console.log('Welcome to your contacts manager!')
+}
+
+function showAll(contacts) {
+    for(let i = 0; i < contacts.length; i++) {
+        console.log(`Last name: ${contacts[i].lastName}, first name: ${contacts[i].firstName}`)
+    }
+}
+
+function addContact() {
+    // Get user input data
+    const firstName = prompt("What's the first name?")
+    const lastName = prompt("What's the last name?")
+    // Add new object to the Array
+    const newContact = {
+        firstName: firstName,
+        lastName: lastName
+    }
+    contacts.push(newContact)
+}
+
+function quit() {
+    if(confirm('You are going to stop the "Contact Manager" App\nAre you sure?')) {
+        console.log('App stopped successfully!')
+    } else {
+        this.userOptions()
+    }
+}
+
+function userOptions() {
+    console.log('1: List All Contacts \n2: Add Contact\n0: Quit')
+    
+    let userInput
+    do {
+        userInput = prompt("What's your choice?")
+    } while(!userInput)
+    
+
+    switch(userInput) {
+        case '0':
+            quit()
+        break;
+        case '1':
+            showAll(contacts)
+            this.userOptions()
+        break;
+        case '2':
+            addContact()
+            this.userOptions()
+        break;
+    }
+}
+
+init()
